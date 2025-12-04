3d6bbd072d5ef69f7dd02a076fb3194379b3cea1
Batu Eren <batu.eren2004@gmail.com>
2025-12-04 17:08:15 +0100

    tree elevate bug fixed, tree design improved

--- project/tree.py
+++ project/tree.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 from framework.renderer import *
 from framework.shapes import Cylinder, Cone
@@ -6,24 +7,43 @@ from framework.objects import MeshObject
 from framework.materials import Material
 from pyglm import glm
 
-def build_tree(position = glm.vec3(0.0, 0., 0.0), height = 4, width = 4, segments= 4): # return tree obj(s)
+
+def build_tree(position = glm.vec3(0.0, 0., 0.0), height = 4, width = 4, segments= 4):
     objs = []
 
-    log_mesh = Cylinder(radius= width/4.0, height = height/3.0, color=glm.vec4(0.8, 0.5, 0.3, 1.0))
-    log_mat = Material()
-    log_transform = glm.translate(position) * glm.translate(glm.vec3(0.0, height * 0.33, 0.0))
-    log_obj = MeshObject(log_mesh, log_mat, transform=log_transform)
+    trunk_height   = height / 5.0
+    foliage_height = height - trunk_height
 
+    log_mesh = Cylinder(
+        radius = width / 4.0,
+        height = trunk_height,
+        color  = glm.vec4(0.8, 0.5, 0.3, 1.0)
+    )
+    log_mat = Material()
+
+    log_transform = glm.translate(position + glm.vec3(0.0, trunk_height * 0.5, 0.0))
+    log_obj = MeshObject(log_mesh, log_mat, transform=log_transform)
     objs.append(log_obj)
 
-    for i in range(1, segments+1):
-        cone_mesh = Cone(color=glm.vec4(0.2, 0.9, 0.2, 1.0), radius=width/max(1, math.log(i)), height=height*0.66 / segments)
-        cone_transform = glm.translate(position) * glm.translate(glm.vec3(0.0, height*0.33 + (i*height*0.5/segments), 0.0))
+    per_segment = foliage_height / float(segments)
+
+    for i in range(segments):
+        cone_height = per_segment
+        cone_radius = width / max(1.0, math.log(i + 2))  # avoid log(1)=0
+
+        colorRand = random.random()
+
+        cone_mesh = Cone(
+            color  = glm.vec4(0.2+0.4*colorRand, 0.8+0.2*colorRand, 0.2+0.05*colorRand, 1.0),
+            radius = cone_radius,
+            height = cone_height
+        )
         cone_mat = Material()
+
+        cone_center_y = trunk_height + cone_height*0.7 * (0.5 + i)
+
+        cone_transform = glm.translate(position + glm.vec3(0.0, cone_center_y, 0.0))
         cone_obj = MeshObject(cone_mesh, cone_mat, transform=cone_transform)
         objs.append(cone_obj)
-
-    #MeshObject(mesh=log_mesh, material=cube_mat, transform=cube_transform)
-    #floor_obj.transform = glm.rotate(glm.radians(-90), glm.vec3(1, 0, 0))
 
     return objs
